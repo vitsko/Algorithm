964b1094409bd464ff8b5059171463d383d7c5a7
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2017-07-02 20:31:58 +0400

    Pre-selection. Part 1. Task 2.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using static System.Console;
+
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            List<string> forSorting = new List<string>();
+            byte beforLastString = 2;
+
+            Title = Text.Title;
+
+            if (Screen.IsExistData(forSorting))
+            {
+                Screen.Show(Text.AboutData, forSorting);
+                var afterSorting = forSorting.OrderBy(item => item.Length);
+                Screen.Show(Text.AfterSorting, afterSorting.ToList());
+
+                var secondByLength = afterSorting.ElementAt(afterSorting.Count() - beforLastString);
+                Screen.Show(Text.SecondString, secondByLength);
+            }
+            else
+            {
+                if (!Screen.IsException)
+                {
+                    Screen.Show(string.Format(Text.EmptyFile, Screen.FullPathToFile));
+                }
+            }
+
+            Screen.Show(Text.PressAnyKey);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Screen.cs
@@ -0,0 +1,78 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using static System.Console;
+
+    internal static class Screen
+    {
+        internal static string FullPathToFile { get; private set; }
+
+        internal static bool IsException { get; private set; }
+
+        internal static bool IsExistData(List<string> result)
+        {
+            StreamReader reader = null;
+            FullPathToFile = string.Format(Text.FullPathToFile, Environment.CurrentDirectory, Text.FileWithStrings);
+
+            try
+            {
+                reader = new StreamReader(FullPathToFile);
+
+                while (reader.Peek() >= 0)
+                {
+                    var oneString = reader.ReadLine();
+
+                    if (result == null)
+                    {
+                        result = new List<string>();
+                    }
+
+                    result.Add(oneString);
+                }
+
+                if (result.Count == 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                IsException = true;
+                Screen.Show(ex.Message);
+            }
+
+            return false;
+        }
+
+        internal static void Show(string title, List<string> textToShow)
+        {
+            WriteLine(string.Empty);
+            WriteLine(title);
+
+            foreach (var stringToShow in textToShow)
+            {
+                WriteLine(stringToShow);
+            }
+        }
+
+        internal static void Show(params string[] textToShow)
+        {
+            WriteLine(string.Empty);
+
+            foreach (var stringToShow in textToShow)
+            {
+                WriteLine(stringToShow);
+            }
+        }
+
+        internal static void Show(string message)
+        {
+            WriteLine(string.Empty);
+            WriteLine(message);
+        }
+    }
+}
